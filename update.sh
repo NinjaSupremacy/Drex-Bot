fc71a2093712a4ff0aa0ce48a49cfdcb58c98f9f
Ninja Spmc <ninjaspmc@gmail.com>
2023-10-13 15:36:57 -0300

    primeiro commit

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-zip -r banco de dados.zip banco de dados && mv banco de dados.zip ../ && cd .. && rm -rf Drex-Bot && git clone https://github.com/NinjaSupremacy/Drex-MD.git && rm -rf ./Drex-Bot/banco de dados && mv banco de dados.zip Drex-Bot && cd Drex-Bot && unzip banco de dados.zip && sh start.sh
+zip -r "banco de dados.zip" "banco de dados" && mv "banco de dados.zip" ../ && cd .. && rm -rf Drex-Bot && git clone https://github.com/NinjaSupremacy/Drex-MD.git && rm -rf ./Drex-Bot/"banco de dados" && mv "banco de dados.zip" Drex-Bot && cd Drex-Bot && unzip "banco de dados.zip" && sh drex.sh
