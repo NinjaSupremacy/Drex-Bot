71a95af0af5cf031da241d603107e630d1e968ad
Ninja Spmc <ninjaspmc@gmail.com>
2023-11-02 10:55:50 -0300

    primeiro commit

--- configuracoes/lib/ownerpix.js
+++ configuracoes/lib/ownerpix.js
@@ -1,11 +1,11 @@
-const ownerpix = (numerodono) => {
+const ownerpix = (settings) => {
 return`Apenas para doações.*
 
 💠 Chave Pix do meu dono:
 (91) 98257-7902
 
 Envie o *comprovante de pagamento* para: 
-wa.me/${numerodono}`
+wa.me/${settings.numerodono}`
 }
 
 exports.ownerpix = ownerpix
